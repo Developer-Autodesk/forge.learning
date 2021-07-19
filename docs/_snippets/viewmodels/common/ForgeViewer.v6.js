1780c6915638e081eefad30d4aa701d88d6d6d00
Petr Broz <ipetrbroz@gmail.com>
2021-07-19 13:27:30 +0200

    Refactored snippets for 2LO and 3LO viewer setup.

---
+++ docs/_snippets/viewmodels/common/ForgeViewer.v6.js
@@ -0,0 +1,59 @@
+var viewerApp;
+
+function launchViewer(urn) {
+  if (viewerApp != null) {
+    var thisviewer = viewerApp.getCurrentViewer();
+    if (thisviewer) {
+      thisviewer.tearDown()
+      thisviewer.finish()
+      thisviewer = null
+      $("#forgeViewer").empty();
+    }
+  }
+
+  var options = {
+    env: 'AutodeskProduction',
+    getAccessToken: getForgeToken
+  };
+  var documentId = 'urn:' + urn;
+  Autodesk.Viewing.Initializer(options, function onInitialized() {
+    viewerApp = new Autodesk.Viewing.ViewingApplication('forgeViewer');
+    viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
+    viewerApp.loadDocument(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+  });
+}
+
+function onDocumentLoadSuccess(doc) {
+  // We could still make use of Document.getSubItemsWithProperties()
+  // However, when using a ViewingApplication, we have access to the **bubble** attribute,
+  // which references the root node of a graph that wraps each object from the Manifest JSON.
+  var viewables = viewerApp.bubble.search({ 'type': 'geometry' });
+  if (viewables.length === 0) {
+    console.error('Document contains no viewables.');
+    return;
+  }
+
+  // Choose any of the available viewables
+  viewerApp.selectItem(viewables[0].data, onItemLoadSuccess, onItemLoadFail);
+}
+
+function onDocumentLoadFailure(viewerErrorCode, viewerErrorMsg) {
+  console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode + '\n- errorMessage:' + viewerErrorMsg);
+}
+
+function onItemLoadSuccess(viewer, item) {
+  // item loaded, any custom action?
+}
+
+function onItemLoadFail(errorCode) {
+  console.error('onItemLoadFail() - errorCode:' + errorCode);
+}
+
+function getForgeToken(callback) {
+  jQuery.ajax({
+    url: '/api/forge/oauth/token',
+    success: function (res) {
+      callback(res.access_token, res.expires_in)
+    }
+  });
+}
