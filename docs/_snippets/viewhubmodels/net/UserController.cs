4c5c190cb5a4db01985923e1d0efb96e017b73a9
Petr Broz <ipetrbroz@gmail.com>
2021-07-19 12:48:30 +0200

    Refactored snippets for View Hub Models (.NET).

---
+++ docs/_snippets/viewhubmodels/net/UserController.cs
@@ -0,0 +1,35 @@
+using Autodesk.Forge;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace forgesample.Controllers
+{
+  public class UserController : ApiController
+  {
+    [HttpGet]
+    [Route("api/forge/user/profile")]
+    public async Task<JObject> GetUserProfileAsync()
+    {
+      Credentials credentials = await Credentials.FromSessionAsync();
+      if (credentials == null)
+      {
+        return null;
+      }
+
+      // the API SDK
+      UserProfileApi userApi = new UserProfileApi();
+      userApi.Configuration.AccessToken = credentials.TokenInternal;
+
+
+      // get the user profile
+      dynamic userProfile = await userApi.GetUserProfileAsync();
+
+      // prepare a response with name & picture
+      dynamic response = new JObject();
+      response.name = string.Format("{0} {1}", userProfile.firstName, userProfile.lastName);
+      response.picture = userProfile.profileImages.sizeX40;
+      return response;
+    }
+  }
+}
